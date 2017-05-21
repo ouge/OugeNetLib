733cddf35daf67934ccf45d4ae61346c77ca02e7
ou <1093520047@qq.com>
2017-05-21 21:03:11 +0800

    ok

--- base/Exception.h
+++ base/Exception.h
@@ -7,18 +7,18 @@
 namespace ouge {
 
 class Exception : public std::exception {
- public:
-  explicit Exception(const char* what);
-  explicit Exception(const std::string& what);
-  virtual ~Exception() throw();
-  virtual const char* what() const throw();
-  const char* stackTrace() const throw();
+  public:
+    explicit Exception(const char* what);
+    explicit Exception(const std::string& what);
+    virtual ~Exception() throw();
+    virtual const char* what() const throw();
+    const char*         stackTrace() const throw();
 
- private:
-  void fillStackTrace();
+  private:
+    void fillStackTrace();
 
-  std::string message_;
-  std::string stack_;
+    std::string message_;
+    std::string stack_;
 };
 }
 
--- base/Singleton.h
+++ base/Singleton.h
@@ -14,48 +14,48 @@ namespace detail {
 // 判断一个类型有没有no_destroy成员
 template <typename T>
 struct has_no_destroy {
-  template <typename C>
-  static char test(decltype(&C::no_destroy));
-  template <typename C>
-  static int32_t test(...);
+    template <typename C>
+    static char test(decltype(&C::no_destroy));
+    template <typename C>
+    static int32_t test(...);
 
-  // 如果value为true，说明调用了test(decltype(&C::no_destroy)
-  // sizeof（函数） 不需要函数实现，只需要函数返回值。
-  const static bool value = sizeof(test<T>(0)) == 1;
+    // 如果value为true，说明调用了test(decltype(&C::no_destroy)
+    // sizeof（函数） 不需要函数实现，只需要函数返回值。
+    const static bool value = sizeof(test<T>(0)) == 1;
 };
-}  // ouge::detail
+}    // ouge::detail
 
 template <typename T>
 class Singleton : NonCopyable {
- public:
-  static T& instance() {
-    pthread_once(&ponce_, &Singleton::init);
-    assert(value_ != NULL);
-    return *value_;
-  }
-
- private:
-  Singleton();
-  ~Singleton();
-
-  static void init() {
-    value_ = new T();
-    if (!detail::has_no_destroy<T>::value) {
-      ::atexit(destroy);
+  public:
+    static T& instance() {
+        pthread_once(&ponce_, &Singleton::init);
+        assert(value_ != NULL);
+        return *value_;
     }
-  }
 
-  static void destroy() {
-    using T_must_be_complete_type = char[sizeof(T) == 0 ? -1 : 1];
-    T_must_be_complete_type dummy;
-    (void)dummy;
-    delete value_;
-    value_ = NULL;
-  }
+  private:
+    Singleton();
+    ~Singleton();
 
- private:
-  static pthread_once_t ponce_;
-  static T* value_;
+    static void init() {
+        value_ = new T();
+        if (!detail::has_no_destroy<T>::value) {
+            ::atexit(destroy);
+        }
+    }
+
+    static void destroy() {
+        using T_must_be_complete_type = char[sizeof(T) == 0 ? -1 : 1];
+        T_must_be_complete_type dummy;
+        (void)dummy;
+        delete value_;
+        value_ = NULL;
+    }
+
+  private:
+    static pthread_once_t ponce_;
+    static T*             value_;
 };
 
 template <typename T>
@@ -63,6 +63,6 @@ pthread_once_t Singleton<T>::ponce_ = PTHREAD_ONCE_INIT;
 
 template <typename T>
 T* Singleton<T>::value_ = NULL;
-}  // ouge
+}    // ouge
 
 #endif /* SINGLETON_H */
